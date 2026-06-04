796946a1cd335317157715abde100c7eb8b77cfa
Zakaria Abdi <zakariaabdisheikh004@gmail.com>
2026-06-04 18:02:06 +0300

    Input updated

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,9 +1,20 @@
 // Code PasswordInput Component Here
+import React from 'react';
 
-function PasswordInput (){
-    return(
-        <></>
-    )
+function PasswordInput() {
+  const handleChange = (event) => {
+    console.log("Entering password...");
+  };
+
+  return (
+    <div>
+      <input 
+        type="password" 
+        onChange={handleChange} 
+        placeholder="Enter your password"
+      />
+    </div>
+  );
 }
 
 export default PasswordInput;
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,9 +1,28 @@
 // Code SubmitButton Component Here
+import React from 'react';
 
-function SubmitButton (){
-    return(
-        <></>
-    )
+function SubmitButton() {
+  // Task 3: Create handleEnter function
+  const handleEnter = () => {
+    console.log("Mouse Entering");
+  };
+
+  // Task 3: Create handleLeave function
+  const handleLeave = () => {
+    console.log("Mouse Exiting");
+  };
+
+  return (
+    <div>
+      {/* Task 3: Render button, exact text, and attach mouse events */}
+      <button 
+        onMouseEnter={handleEnter} 
+        onMouseLeave={handleLeave}
+      >
+        Submit Password
+      </button>
+    </div>
+  );
 }
 
 export default SubmitButton;
